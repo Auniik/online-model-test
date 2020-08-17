f2c297601fe012e0c21c2981dfb6f9ae9aa7aad6
auniik <aunik.t2@gmail.com>
2020-08-17 23:16:19 +0600

    Added adsense script

--- resources/views/front/layout/master.blade.php
+++ resources/views/front/layout/master.blade.php
@@ -39,6 +39,7 @@
         }
     </style>
     @stack('style')
+    <script data-ad-client="ca-pub-8792021099877651" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 </head>
 
 <body class="content" id="body">
